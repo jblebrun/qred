abe006d6d51c20e31c8b98e6cca441f31a2a459d
Jason LeBrun <jblebrun@gmail.com>
2013-05-04 19:49:44 -0700

    Fix up things to get first basic test running

--- test/simple.js
+++ test/simple.js
@@ -1,16 +1,20 @@
 var assert = require("assert");
-var Qred = require("../lib/qred");
+var Qred = require("../lib/qred").Qred;
+var redis = require("redis");
 
+var client = redis.createClient(6480);
+var sclient = redis.createClient(6480);
 var tests = [ 
     function singleJob(done) {
-        var q = new Qred({
+        var q = new Qred(client, sclient, {
+            log: console.log.bind(console),
             handler: function(data, callback) {
                 callback(null, JSON.stringify(data));
             }
         });
         var data = { data1: "a", data2: "b" };
         q.submitJob("ajobid", data , {}, function(err, result) {
-            assert(!err);
+            assert(!err, err);
             assert(result == JSON.stringify(data));
             done();
         });
